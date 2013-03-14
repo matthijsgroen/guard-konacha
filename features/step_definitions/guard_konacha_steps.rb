6cdbc8aea13709b9a8f6f63e27c30cfe7aa8c3fc
Matthijs Groen <matthijs.groen@gmail.com>
2013-03-14 21:21:40 +0100

    Start of step implementation

--- features/step_definitions/guard_konacha_steps.rb
+++ features/step_definitions/guard_konacha_steps.rb
@@ -1,5 +1,29 @@
+require 'guard/konacha'
+
+Before do
+
+  module Guard::UI
+    class << self
+      def info(message, options = {})
+      end
+    end
+  end
+
+  @guard_konacha = Guard::Konacha.new
+end
+
 Given(/^I have a failing spec file$/) do
-  pending # express the regexp above with the code you wish you had
+  @guard_konacha.runner.stub(:session).and_return(
+    double('session',
+           :reset! => true,
+           :visit => true,
+           :evaluate_script => 'object'
+          )
+  )
+  @guard_konacha.runner.run [
+    'failing_spec.rb'
+  ]
+  pending
 end
 
 When(/^I make the spec file pass$/) do
